c66fdf4cd75223f51ec60b3720dff5357c5cb5fe
Antoine Galopin <antoine.galopin@etu.univ-lehavre.fr>
2023-01-20 14:50:52 +0100

    Ajout de méthodes TablierSolitaire

--- TablierSolitaire.php
+++ TablierSolitaire.php
@@ -1,25 +1,55 @@
 <?php
+	class TablierSolitaire {
+		const NORD = 0;
+		const EST = 0;
+		const SUD = 0;
+		const OUEST = 0;
 
+		private $nbLignes;
+		private $nbColonnes;
 
-class TablierSolitaire
-{
+		private $tablier;
+		
+		private function TablierSolitaire($nblig = 5, $nbcol = 5) {
+			$this->nbLignes = $nblig;
+			$this->nbColonnes = $nbcol;
+			$this->tablier = array();
 
-	const NORD = 0;
-	const EST = 0;
-	const SUD = 0;
-	const OUEST = 0;
+			for( $i = 0; $i < $this->nbLignes; $i++ ) {
+				$this->tablier[$i] = array();
 
-	private $nbLignes;
-	private $nbColonnes;
-	
-	function __construct($nblig=5, $nbcol=5)
-	{
-		echo $nblig, $nbcol;
+				for( $j = 0; $j < $this->nbColonnes; $j++ ) {
+					$this->tablier[$i][$j] = new CaseSolitaire();
+				}
+			}
+		}
+
+		public function getNbLignes() {
+			return $this->nbLignes;
+		}
+
+		public function getNbColonnes() {
+			return $this->nbColonnes;
+		}
+
+		public function getTablier() {
+			return $this->tablier;
+		}
+
+		public function getCase($ligne, $colonne) {
+			return $this->tablier[$ligne][$colonne];
+		}
+
+		public function videCase($ligne, $colonne) {
+			$this->tablier[$ligne][$colonne]->setValeur(CaseSolitaire::VIDE);
+		}
+
+		public function remplitCase($ligne, $colonne) {
+			$this->tablier[$ligne][$colonne]->setValeur(CaseSolitaire::BILLE);
+		}
+
+		public function neutraliseCase($ligne, $colonne) {
+			$this->tablier[$ligne][$colonne]->setValeur(CaseSolitaire::NEUTRALISE);
+		}
 	}
-}
-
-
-$tablier = new TablierSolitaire();
-
-
 ?>
